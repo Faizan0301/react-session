9fa8f0aa0a558bccaf7efe327d75a8a6c1f434e7
unknown <Faizan0301>
2024-10-04 01:26:06 +0530

    upload'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Curd from './components/Curd'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Curd />
+  )
+}
+
+export default App
---
+++ src/components/Curd.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+
+function Curd() {
+
+    let [user, setUser] = useState({});
+    let [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        let oldData = JSON.parse(sessionStorage.getItem("users")) || [];
+        setUserData(oldData)
+    }, [])
+
+    let handelChange = (e) => {
+        let { name, value } = e.target;
+        setUser({ ...user, [name]: value })
+    }
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        let data = [...userData, user];
+        setUserData(data);
+        sessionStorage.setItem("users", JSON.stringify(data))
+        setUser({})
+    }
+
+    let handleDelete = (idx) => {
+        let data =[...userData] ;
+        data.splice(idx,1);
+        setUserData(data)
+        sessionStorage.setItem("users", JSON.stringify(data))
+    }
+
+    let handleEdit=(idx)=>{
+        setUser(userData[idx])
+    }
+    return (
+        <>
+            <div className="container">
+                <form className="form" method='post' onSubmit={ handleSubmit }>
+                    <h2>Hey bub, ya gotta register!</h2>
+                    <div className="form-control">
+                        <label htmlFor="username">Name</label>
+                        <input type="text" name="name" onChange={ handelChange } value={user ? user.name : ""} placeholder="Enter name" />
+                        <small>Error message</small>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" onChange={ handelChange } value={user ? user.email : ""} placeholder="Enter email" />
+                        <small>Error message</small>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Phone</label>
+                        <input type="text" name="phone" onChange={ handelChange } value={user ? user.phone : ""} placeholder="Enter Phone" />
+                        <small>Error message</small>
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password2">Gender</label>
+                        <input type="radio" name='gender' onChange={ handelChange } checked={user && user.gender=="male"} value='male' />Male
+                        <input type="radio" name='gender' onChange={ handelChange } checked={user && user.gender=="female"} value='female' />Female
+                        <small>Error message</small>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <section>
+                <h1>Fixed Table header</h1>
+                <div className="tbl-header">
+                    <table cellPadding="0" cellSpacing="0" border="0">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Gender</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+                <div className="tbl-content">
+                    <table cellPadding="0" cellSpacing="0" border="0">
+                        <tbody>
+                            {
+                                userData.map((val, idx) => (
+                                    <tr>
+                                        <td>{ val.name }</td>
+                                        <td>{ val.email } COMPANY </td>
+                                        <td>{ val.phone }</td>
+                                        <td>{ val.gender }</td>
+                                        <td>
+                                            <button className='delete' onClick={ () => handleDelete(idx) }>Delete</button>
+                                            <button className='edit' onClick={()=>handleEdit(idx)}>Edit</button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Curd
