7b42c335e948322bd559c621555cf165d75faa65
unknown <Faizan0301>
2024-10-04 01:57:01 +0530

    upload

--- src/components/Curd.jsx
+++ src/components/Curd.jsx
@@ -4,6 +4,7 @@ function Curd() {
 
     let [user, setUser] = useState({});
     let [userData, setUserData] = useState([]);
+    let [index, setIndex] = useState(-1);
 
     useEffect(() => {
         let oldData = JSON.parse(sessionStorage.getItem("users")) || [];
@@ -17,20 +18,28 @@ function Curd() {
 
     let handleSubmit = (e) => {
         e.preventDefault();
-        let data = [...userData, user];
+        let data
+        if (index != -1) {
+            data = [...userData]
+            data[index] = user;
+        } else {
+            data = [...userData, user];
+        }
         setUserData(data);
         sessionStorage.setItem("users", JSON.stringify(data))
         setUser({})
+        setIndex(-1)
     }
 
     let handleDelete = (idx) => {
-        let data =[...userData] ;
-        data.splice(idx,1);
+        let data = [...userData];
+        data.splice(idx, 1);
         setUserData(data)
         sessionStorage.setItem("users", JSON.stringify(data))
     }
 
-    let handleEdit=(idx)=>{
+    let handleEdit = (idx) => {
+        setIndex(idx)
         setUser(userData[idx])
     }
     return (
@@ -40,26 +49,26 @@ function Curd() {
                     <h2>Hey bub, ya gotta register!</h2>
                     <div className="form-control">
                         <label htmlFor="username">Name</label>
-                        <input type="text" name="name" onChange={ handelChange } value={user ? user.name : ""} placeholder="Enter name" />
+                        <input type="text" name="name" onChange={ handelChange } value={ user.name || "" } placeholder="Enter name" />
                         <small>Error message</small>
                     </div>
                     <div className="form-control">
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" onChange={ handelChange } value={user ? user.email : ""} placeholder="Enter email" />
+                        <input type="text" name="email" onChange={ handelChange } value={ user.email || "" } placeholder="Enter email" />
                         <small>Error message</small>
                     </div>
                     <div className="form-control">
                         <label htmlFor="password">Phone</label>
-                        <input type="text" name="phone" onChange={ handelChange } value={user ? user.phone : ""} placeholder="Enter Phone" />
+                        <input type="text" name="phone" onChange={ handelChange } value={ user.phone || "" } placeholder="Enter Phone" />
                         <small>Error message</small>
                     </div>
                     <div className="form-control">
                         <label htmlFor="password2">Gender</label>
-                        <input type="radio" name='gender' onChange={ handelChange } checked={user && user.gender=="male"} value='male' />Male
-                        <input type="radio" name='gender' onChange={ handelChange } checked={user && user.gender=="female"} value='female' />Female
+                        <input type="radio" name='gender' onChange={ handelChange } checked={ user && user.gender == "male" } value='male' />Male
+                        <input type="radio" name='gender' onChange={ handelChange } checked={ user && user.gender == "female" } value='female' />Female
                         <small>Error message</small>
                     </div>
-                    <button type="submit">Submit</button>
+                    <button type="submit">{ !index ? "Update" : "Submit" }</button>
                 </form>
             </div>
             <section>
@@ -84,12 +93,12 @@ function Curd() {
                                 userData.map((val, idx) => (
                                     <tr>
                                         <td>{ val.name }</td>
-                                        <td>{ val.email } COMPANY </td>
+                                        <td>{ val.email }</td>
                                         <td>{ val.phone }</td>
                                         <td>{ val.gender }</td>
                                         <td>
                                             <button className='delete' onClick={ () => handleDelete(idx) }>Delete</button>
-                                            <button className='edit' onClick={()=>handleEdit(idx)}>Edit</button>
+                                            <button className='edit' onClick={ () => handleEdit(idx) }>Edit</button>
                                         </td>
                                     </tr>
                                 ))
